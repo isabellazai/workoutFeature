dfe446c1fb7da31a9cf954f80bcb9d03d998fd6d
Isabella <isabellawzai@gmail.com>
2023-08-17 09:57:16 -0400

    Initial Commit

---
+++ workoutFeature/workoutFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  workoutFeatureApp.swift
+//  workoutFeature
+//
+//  Created by scholar on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct workoutFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
